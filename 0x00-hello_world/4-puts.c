ff7d2cc87b91790566f85397e1073042edf2da6d
IfeoluwaOkesola <atoyebiife@gmail.com>
2022-03-11 17:17:34 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,10 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
  * Return: Always 0 (success)
-=======
- * Return: Always 0 (Success)
->>>>>>> 3d6478f5bcad9199fd45d708b1cda972758dd17d
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 3d6478f5bcad9199fd45d708b1cda972758dd17d
