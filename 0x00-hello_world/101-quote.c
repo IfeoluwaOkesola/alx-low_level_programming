9c5240adbf83b53987217d6a334ff9c1a8bb2715
IfeoluwaOkesola <atoyebiife@gmail.com>
2022-03-13 14:08:38 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';)
+		putchar(ch);
+
+	return (1);
+}
